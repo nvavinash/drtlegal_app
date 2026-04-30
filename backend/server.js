a5a51fa889f12eee692a70cc68d1a547d579cb17
nvavinash <avinashmk42@gmail.com>
2026-04-30 15:36:52 +0530

    cors fixed

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+  origin: "https://drtlegal-app.vercel.app",
+  credentials: true
+}
+));
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
